0bbdb4982b28da6e97464e7f08a99f3d9837a6d0
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-22 21:38:25 +0200

    Navbar "prototype"

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,9 @@
 </template>
 
 <style lang="scss">
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
   <div class="home">
     <PageBackground />
     <Navbar />
-    <h1>This is a home page</h1>
   </div>
 </template>
 
