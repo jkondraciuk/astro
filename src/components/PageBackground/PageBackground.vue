c9a1fcd50ec306d182cb2ab2cd2243866f08bc56
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-22 21:20:54 +0200

    Added PageBackground component

---
+++ src/components/PageBackground/PageBackground.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="page-background">
+    </div>
+</template>
+<script>
+export default {
+    name: 'PageBackground'
+}
+</script>
+<style src="./PageBackground.scss" lang="scss" scoped />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
     <h1>This is a home page</h1>
+    <PageBackground />
   </div>
 </template>
 
 <script>
+import PageBackground from '@/components/PageBackground/PageBackground'
 
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    PageBackground
+  }
 }
 </script>
