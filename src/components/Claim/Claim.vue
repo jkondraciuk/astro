76bd1866658090187a10820217e05ec5c448f268
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-23 09:15:07 +0200

    Created and mounted Claim component

---
+++ src/components/Claim/Claim.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="claim-wrapper">
+    </div>
+</template>
+<script>
+export default {
+    name: 'Claim'
+}
+</script>
+<style src="./Claim.scss" lang="scss" scoped />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <PageBackground />
     <Navbar />
+    <Claim />
   </div>
 </template>
 
 <script>
 import PageBackground from '@/components/PageBackground/PageBackground'
 import Navbar from '@/components/Navbar/Navbar'
+import Claim from '@/components/Claim/Claim'
 
 export default {
   name: 'home',
   components: {
     PageBackground,
-    Navbar
+    Navbar,
+    Claim
   }
 };
 </script>
