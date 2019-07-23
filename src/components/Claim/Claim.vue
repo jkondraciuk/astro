8b405d29d90fd233ac823bf6a1afef289977a4ad
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-23 09:28:04 +0200

    Updated Claim content

--- src/components/Claim/Claim.vue
+++ src/components/Claim/Claim.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="claim-wrapper">
+        <h1 class="claim-wrapper__header">ASTRO</h1>
+        <p class="claim-wrapper__paragraph">Begin your journey throughour amazing galaxy, and discover places you never even heard of.</p>
+        <p class="claim-wrapper__paragraph">Type anything space-related to start.</p>
     </div>
 </template>
 <script>
