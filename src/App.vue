f24669ce07148098ea4549e414aa3ab7b8ed6f74
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-24 10:54:47 +0200

    Added 'Roboto' font

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@
 </template>
 
 <style lang="scss">
+  /* Fonts */
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap');
+
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: 'Roboto';
     font-size: 62.5%;
+    font-weight: 300;
   }
 
   body {
