5d539ae88dc0bd57901e125be57bdde3c2a1dd09
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-30 17:17:55 +0200

    Added some content to About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Navbar />
+    <div class="about__main">
+      <h1>This is an about page</h1>
+      <h2>This is a subheader</h2>
+      <div class="about__infos">
+        <InfoBox />
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import Navbar from '@/components/Navbar/Navbar';
+
 export default {
-  name: 'About'
+  name: 'About',
+  components: {
+    Navbar
+  }
 }
 </script>
 <style lang="scss">
@@ -13,5 +25,15 @@ export default {
     background-image: url('../assets/img/about_background.jpg');
     background-size: cover;
     background-attachment: fixed;
+  }
+
+  .about {
+    &__main {
+      width: 80%;
+      margin: 0 auto;
+      margin-top: 70vh;
+      background-color: $white;
+      color: $grey-7;
+    }
   }
 </style>
