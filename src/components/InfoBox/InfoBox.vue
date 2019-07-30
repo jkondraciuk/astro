c840ba7784c600983a28e3e099e8e17a97dd5f22
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-30 17:22:29 +0200

    Added InfoBox component

---
+++ src/components/InfoBox/InfoBox.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="info-wrapper">
+        <h3 class="info-wrapper__header">{{ content.header }}</h3>
+        <p class="info-wrapper__paragraph">{{ content.paragraph }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'InfoBox',
+    props: ['content']
+}
+</script>
+<style src="./InfoBox.scss" lang="scss" scoped />
