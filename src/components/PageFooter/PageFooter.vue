68ce66b7cbbcac725834c5e754b66649c2aa2767
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-07-31 19:30:15 +0200

    Created PageFooter component

---
+++ src/components/PageFooter/PageFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="footer">
+        <p class="footer__text">Footer here - 2019 &copy; - kondraciuk.dev@gmail.com</p>
+    </footer>
+</template>
+<script>
+export default {
+    name: 'PageFooter'
+}
+</script>
+<style src="./PageFooter.scss" lang="scss" scoped />
