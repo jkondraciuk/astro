91029dcc695c578a89a45097dc2410b33a1b5234
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2019-08-04 22:37:09 +0200

    Created ResultImage component

---
+++ src/components/ResultImage/ResultImage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="result-wrapper"></div>
+</template>
+
+<script>
+export default {
+    name: 'ResultImage'
+}
+</script>
+
+<style src="./ResultImage.scss" lang="scss" scoped />
